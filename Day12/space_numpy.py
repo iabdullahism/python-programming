dad9bb1ac5a338dd0718f6795ed35044b23cd904
panditrahulsharma <rahulpandit151197@gmail.com>
2019-06-18 17:37:26 +0530

    :1 q q

---
+++ Day12/space_numpy.py
@@ -0,0 +1,22 @@
+"""
+Code Challenge
+  Name: 
+    Space Seperated data
+  Filename: 
+    space_numpy.py
+  Problem Statement:
+    You are given a 9 space separated numbers. 
+    Write a python code to convert it into a 3x3 NumPy array of integers.
+  Input:
+    6 9 2 3 5 8 1 5 4
+  Output:
+      [[6 9 2]
+      [3 5 8]
+      [1 5 4]]
+  
+"""
+import numpy as np 
+x=[6,9,2,3,5,8,1,5,4]
+x = np.array(x)
+y = x.reshape(3,3)
+print(y)
