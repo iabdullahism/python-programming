47223f989beaaf38145ba5199cc97c458d3dab0e
panditrahulsharma <rahulpandit151197@gmail.com>
2019-07-12 21:11:03 +0530

    RNN

---
+++ sites.py
@@ -0,0 +1,18 @@
+analytics vidhya
+kdnuggets
+kaggle compection
+machinehack compection
+machine learning mastry
+sebastian raschka
+
+uci repo
+"""
+youtube
+statsquats
+"""
+
+"""
+cnn
+
+https://medium.com/@RaghavPrabhu/understanding-of-convolutional-neural-network-cnn-deep-learning-99760835f148
+"""
